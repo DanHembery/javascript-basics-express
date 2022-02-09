c7890d3e49c8c4b6a405c6413ff5f817589b3183
DanHembery <93157414+DanHembery@users.noreply.github.com>
2022-02-09 17:49:28 +0000

    passed all strings tests

--- src/app.js
+++ src/app.js
@@ -1,9 +1,35 @@
 const express = require('express');
+const { 
+    sayHello,
+    uppercase,
+    lowercase,
+    firstCharacter,
+    firstCharacters
+} = require('./lib/strings');
+
 
 const app = express();
 
-app.get('/strings/hello/world', (req, res) => {
-    res.status(200).json({ result: 'Hello, world!' });
+app.get('/strings/hello/:string', (req, res) => {
+    res.status(200).json({ result: sayHello(req.params.string) });
+});
+
+app.get('/strings/upper/:string', (req, res) => {
+    res.status(200).json({ result: uppercase(req.params.string) });
+});
+
+app.get('/strings/lower/:string', (req, res) => {
+    res.status(200).json({ result: lowercase(req.params.string) });
+});
+
+app.get('/strings/first-character/:string', (req, res) => {
+    res.status(200).json({ result: firstCharacter(req.params.string) });
+});
+
+app.get('/strings/first-characters/:string', (req, res) => {
+    const { string } = req.params;
+    const { length } = req.query;
+    res.status(200).json({ result: firstCharacters(string, length) });
 });
 
 module.exports = app;
